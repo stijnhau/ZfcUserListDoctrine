964e9f7113d0211809f0f0c327e8f510fca1ed45
Stijn Haulotte <stijnhau@gmail.com>
2014-08-16 18:33:41 +0200

    Initial commit

---
+++ config/services.config.php
@@ -0,0 +1,32 @@
+<?php
+use Zend\ServiceManager\ServiceLocatorInterface;
+use ZfcUser\Mapper\UserHydrator;
+use ZfcUserList\Options;
+
+return array(
+    'factories' => array(
+        'zfcuser_user_mapper' => function (ServiceLocatorInterface $sm) {
+            /** @var $config \ZfcUserlist\Options\ModuleOptions */
+            $config = $sm->get('zfcuserlist_module_options');
+            $mapperClass = $config->getUserMapper();
+            if (stripos($mapperClass, 'doctrine') !== false) {
+                $mapper = new $mapperClass(
+                    $sm->get('zfcuser_doctrine_em'),
+                    $sm->get('zfcuser_module_options')
+                );
+            } else {
+                /** @var $zfcUserOptions \ZfcUser\Options\UserServiceOptionsInterface */
+                $zfcUserOptions = $sm->get('zfcuser_module_options');
+
+                /** @var $mapper \ZfcUserlistDoctrine\Mapper\UserZendDb */
+                $mapper = new $mapperClass();
+                $mapper->setDbAdapter($sm->get('zfcuser_zend_db_adapter'));
+                $entityClass = $zfcUserOptions->getUserEntityClass();
+                $mapper->setEntityPrototype(new $entityClass);
+                $mapper->setHydrator(new UserHydrator());
+            }
+
+            return $mapper;
+        },
+    ),
+);
