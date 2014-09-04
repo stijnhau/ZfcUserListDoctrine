ca88fe2785b979dcf213d03562620ad0b26f87a3
Stijn Haulotte <stijnhau@gmail.com>
2014-09-04 17:58:01 +0200

    Fixed issue with dev master of zfcUser

--- config/services.config.php
+++ config/services.config.php
@@ -1,7 +1,5 @@
 <?php
 use Zend\ServiceManager\ServiceLocatorInterface;
-use ZfcUser\Mapper\UserHydrator;
-use ZfcUserList\Options;
 
 return array(
     'factories' => array(
@@ -23,7 +21,7 @@ return array(
                 $mapper->setDbAdapter($sm->get('zfcuser_zend_db_adapter'));
                 $entityClass = $zfcUserOptions->getUserEntityClass();
                 $mapper->setEntityPrototype(new $entityClass);
-                $mapper->setHydrator(new UserHydrator());
+                $mapper->setHydrator($sm->get('zfcuser_user_hydrator'));
             }
 
             return $mapper;
